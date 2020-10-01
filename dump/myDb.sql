1884ed78b4999d7cdd3c78372a1a4febc2582e57
Juan Pablo Domínguez Mayo <72070792+pdominguez-iesrafaelalberti@users.noreply.github.com>
2020-10-01 19:46:22 +0200

    Update myDb.sql

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 01-10-2020 a las 17:13:37
+-- Tiempo de generación: 01-10-2020 a las 17:43:16
 -- Versión del servidor: 8.0.21
 -- Versión de PHP: 7.4.9
 
@@ -24,69 +24,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Authors`
+-- Estructura de tabla para la tabla `Autores`
 --
 
-CREATE TABLE `Authors` (
+CREATE TABLE `Autores` (
   `Id` int NOT NULL,
-  `Name` varchar(70) NOT NULL,
-  `Country` varchar(100) NOT NULL
+  `Nombre` varchar(70) NOT NULL,
+  `Ciudad` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Volcado de datos para la tabla `Authors`
+-- Volcado de datos para la tabla `Autores`
 --
 
-INSERT INTO `Authors` (`Id`, `Name`, `Country`) VALUES
-(1, 'J.D. Salinger', 'USA'),
-(2, 'F. Scott. Fitzgerald', 'USA'),
-(3, 'Jane Austen', 'UK'),
-(4, 'Scott Hanselman', 'USA'),
-(5, 'Jason N. Gaylord', 'USA'),
-(6, 'Pranav Rastogi', 'India'),
-(7, 'Todd Miranda', 'USA'),
-(8, 'Christian Wenz', 'USA');
+INSERT INTO `Autores` (`Id`, `Nombre`, `Ciudad`) VALUES
+(1, 'Manuel Santander Cahué', 'Cádiz'),
+(2, 'Juan Carlos Aragón Becerra', 'Cádiz'),
+(3, 'Manuel Alvarez Seda', 'Sevilla'),
+(4, 'Luis Galán Pérez', 'Puerto de Santa María'),
+(5, 'Juan Rivero Torrejón', 'San Fernando'),
+(6, 'Antonio Martínez Ares', 'Cádiz'),
+(7, 'Antonio Pedro Serrano Álvarez', 'Carmona'),
+(8, 'Joaquín Quiñones', 'Cádiz');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Books`
+-- Estructura de tabla para la tabla `Obras`
 --
 
-CREATE TABLE `Books` (
+CREATE TABLE `Obras` (
   `Id` int NOT NULL,
-  `Title` varchar(50) NOT NULL
+  `Titulo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Volcado de datos para la tabla `Books`
+-- Volcado de datos para la tabla `Obras`
 --
 
-INSERT INTO `Books` (`Id`, `Title`) VALUES
-(1, 'The Catcher in the Rye'),
-(2, 'Nine Stories'),
-(3, 'Franny and Zooey'),
-(4, 'The Great Gatsby'),
-(5, 'Tender id the Night'),
-(6, 'Pride and Prejudice'),
-(7, 'Professional ASP.NET 4.5 in C# and VB');
+INSERT INTO `Obras` (`Id`, `Titulo`) VALUES
+(1, 'La familia Pepperoni (Vendetta)'),
+(2, 'La maldición de la lapa negra'),
+(3, 'Los de Capuchinos'),
+(4, 'Los Ángeles Caídos'),
+(5, 'Los Yesterday'),
+(6, 'Las Pito-Risas'),
+(7, 'Todo');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `BooksAuthors`
+-- Estructura de tabla para la tabla `ObrasAutores`
 --
 
-CREATE TABLE `BooksAuthors` (
-  `AuthorId` int NOT NULL,
-  `BookId` int NOT NULL
+CREATE TABLE `ObrasAutores` (
+  `AutoresId` int NOT NULL,
+  `ObrasId` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Volcado de datos para la tabla `BooksAuthors`
+-- Volcado de datos para la tabla `ObrasAutores`
 --
 
-INSERT INTO `BooksAuthors` (`AuthorId`, `BookId`) VALUES
+INSERT INTO `ObrasAutores` (`AutoresId`, `ObrasId`) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
@@ -98,28 +98,6 @@ INSERT INTO `BooksAuthors` (`AuthorId`, `BookId`) VALUES
 (6, 7),
 (7, 7),
 (8, 7);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Customers`
---
-
-CREATE TABLE `Customers` (
-  `Id` int NOT NULL,
-  `User` varchar(70) NOT NULL,
-  `Password` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `Customers`
---
-
-INSERT INTO `Customers` (`Id`, `User`, `Password`) VALUES
-(1, 'admin', 'admin'),
-(2, 'usuario1', 'usuario1'),
-(3, 'usuario2', 'usuario2'),
-(4, 'usuario3', 'usuario3');
 
 -- --------------------------------------------------------
 
@@ -146,64 +124,53 @@ INSERT INTO `Person` (`id`, `name`) VALUES
 --
 
 --
--- Indices de la tabla `Authors`
+-- Indices de la tabla `Autores`
 --
-ALTER TABLE `Authors`
+ALTER TABLE `Autores`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indices de la tabla `Books`
+-- Indices de la tabla `Obras`
 --
-ALTER TABLE `Books`
+ALTER TABLE `Obras`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indices de la tabla `BooksAuthors`
+-- Indices de la tabla `ObrasAutores`
 --
-ALTER TABLE `BooksAuthors`
-  ADD KEY `AuthorId` (`AuthorId`),
-  ADD KEY `BookId` (`BookId`);
-
---
--- Indices de la tabla `Customers`
---
-ALTER TABLE `Customers`
-  ADD PRIMARY KEY (`Id`);
+ALTER TABLE `ObrasAutores`
+  ADD KEY `AutoresId` (`AutoresId`),
+  ADD KEY `ObrasId` (`ObrasId`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `Authors`
+-- AUTO_INCREMENT de la tabla `Autores`
 --
-ALTER TABLE `Authors`
+ALTER TABLE `Autores`
   MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT de la tabla `Books`
+-- AUTO_INCREMENT de la tabla `Obras`
 --
-ALTER TABLE `Books`
+ALTER TABLE `Obras`
   MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `Customers`
---
-ALTER TABLE `Customers`
-  MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `BooksAuthors`
+-- Filtros para la tabla `ObrasAutores`
 --
-ALTER TABLE `BooksAuthors`
-  ADD CONSTRAINT `BooksAuthors_ibfk_1` FOREIGN KEY (`AuthorId`) REFERENCES `Authors` (`Id`),
-  ADD CONSTRAINT `BooksAuthors_ibfk_2` FOREIGN KEY (`BookId`) REFERENCES `Books` (`Id`);
+ALTER TABLE `ObrasAutores`
+  ADD CONSTRAINT `ObrasAutores_ibfk_1` FOREIGN KEY (`AutoresId`) REFERENCES `Autores` (`Id`),
+  ADD CONSTRAINT `ObrasAutores_ibfk_2` FOREIGN KEY (`ObrasId`) REFERENCES `Obras` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
